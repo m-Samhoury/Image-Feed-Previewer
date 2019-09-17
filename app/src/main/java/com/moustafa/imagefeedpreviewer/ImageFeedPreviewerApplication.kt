bea0e8f37d005706b16391f9350eecc7a20ab54c
MrSamhoury <moustafa.samhoury@gmail.com>
2019-09-18 00:16:05 +0300

    Adding all the needed dependencies into gradle

---
+++ app/src/main/java/com/moustafa/imagefeedpreviewer/ImageFeedPreviewerApplication.kt
@@ -0,0 +1,16 @@
+package com.moustafa.imagefeedpreviewer
+
+import android.app.Application
+
+/**
+ * @author moustafasamhoury
+ * created on Tuesday, 17 Sep, 2019
+ */
+
+class ImageFeedPreviewerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
